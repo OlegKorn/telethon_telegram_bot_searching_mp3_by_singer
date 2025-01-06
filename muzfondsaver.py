96d04ff288b73a93401f0e7f5a8f700d4dce1bca
O <48214249+OlegKorn@users.noreply.github.com>
2025-01-06 17:34:41 +0800

    Update muzfondsaver.py
    fixes

--- muzfondsaver.py
+++ muzfondsaver.py
@@ -19,7 +19,7 @@ from multiprocessing import Pool
 class MuzofondMusicSaver(object):
     '''singleton, i.e. only 1 instance can be created'''
     def __init__(self, musician):
-        self.musician = str(musician).strip()
+        self.musician = musician
         self.search_url = f'https://muzofond.fm/collections/artists/{musician}'
 
     def __new__(cls, e): # without e it demands second argument
@@ -35,8 +35,8 @@ class MuzofondMusicSaver(object):
         try:
             self.request = session.get(self.search_url, headers=Headers(headers=True).generate())
             
-            if self.request.status_code >= 400:
-                return 'Request status code is >= 400'
+            if self.request.status_code != 200:
+                return False
             
             self.soup = bs(self.request.content, 'html.parser')
             return self.soup
@@ -47,32 +47,28 @@ class MuzofondMusicSaver(object):
 
     @functions.timeit
     def get_mp3s_of_author_found_songs(self):
+        mp3s_of_author = []
+
         self.s = self.get_soup()
 
         if not self.s:
-            return False 
+            return f'Error: no such author on muzofond.fm - {self.musician}'
         
-        if self.s != 'Request status code is >= 400':
-            mp3s_of_author = []
+        try:
+            page_items = self.s.find('ul', attrs ={"data-url": f"{self.search_url}"}).find_all('li', class_='item')
             
-            try:
-                page_items = self.s.find('ul', attrs ={"data-url": f"{self.search_url}"}).find_all('li', class_='item')
-            except (TypeError, AttributeError) as ex:
-                return ['Error: no such author', ex]
+            for item in page_items:
+                mp3_link = item.find("li", class_="play").get("data-url")
+                mp3_title = item.find("span", class_="track").text.strip()        
+                mp3_title_cleared = functions.delete_forbidden_chars(mp3_title)
 
-            try:
-                for item in page_items:
-                    mp3_link = item.find("li", class_="play").get("data-url")
-                    mp3_title = item.find("span", class_="track").text.strip()
-                    mp3_title_cleared = functions.delete_forbidden_chars(mp3_title)
+                track_data = mp3_link + ":::" + mp3_title_cleared
+                mp3s_of_author.append(track_data)
 
-                    track_data = mp3_link + ":::" + mp3_title_cleared
-                    mp3s_of_author.append(track_data)
+            return mp3s_of_author
 
-                return mp3s_of_author
-
-            except Exception as ex:
-                return ['Error: finding links of songs', ex]
+        except (Exception, TypeError, AttributeError) as ex:
+            return 'Error: except (Exception, TypeError, AttributeError) as ex: - no such author'
 
 
     @functions.timeit
@@ -89,5 +85,3 @@ class MuzofondMusicSaver(object):
                 f'Clearing metadata:\n {ex}',
                 config.RED
             )
-                
-    
